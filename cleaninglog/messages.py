9d4d96a528d27efd111a738a6f549376d80dfcd6
Richard Neumann <r.neumann@homeinfo.de>
2019-03-14 14:46:54 +0100

    Refac.

--- cleaninglog/messages.py
+++ cleaninglog/messages.py
@@ -1,16 +1,13 @@
 """WSGI Messages."""
 
-from his import HIS_MESSAGE_FACILITY
+from wsgilib import JSONMessage
 
 
 __all__ = ['NO_SUCH_TERMINAL', 'NO_SUCH_USER', 'TERMINAL_NOT_LOCATED']
 
 
-CLEANING_LOG_MESSAGE_DOMAIN = HIS_MESSAGE_FACILITY.domain('cleaninglog')
-CLEANING_LOG_MESSAGE = CLEANING_LOG_MESSAGE_DOMAIN.message
-NO_SUCH_TERMINAL = CLEANING_LOG_MESSAGE(
+NO_SUCH_TERMINAL = JSONMessage(
     'The requested terminal does not exist.', status=404)
-NO_SUCH_USER = CLEANING_LOG_MESSAGE(
-    'The requested user does not exist.', status=404)
-TERMINAL_NOT_LOCATED = CLEANING_LOG_MESSAGE(
+NO_SUCH_USER = JSONMessage('The requested user does not exist.', status=404)
+TERMINAL_NOT_LOCATED = JSONMessage(
     'The requested terminal does not have an address.', status=400)
