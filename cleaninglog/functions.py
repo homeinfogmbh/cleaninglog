efc416e0df6cccd5ab06905f085e5fcda86d3cd4
Richard Neumann <r.neumann@homeinfo.de>
2019-10-08 11:45:03 +0200

    Refactored ACCEPT evaluation.

--- cleaninglog/functions.py
+++ cleaninglog/functions.py
@@ -14,7 +14,7 @@ __all__ = ['make_response', 'by_deployment']
 def make_response(cleaning_dates):
     """Creates a response from the respective dictionary."""
 
-    if 'application/json' in ACCEPT or '*/*' in ACCEPT:
+    if 'application/json' in ACCEPT:
         return JSON([
             cleaning_date.to_json(short=True)
             for cleaning_date in cleaning_dates])
