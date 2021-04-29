b27e3eb9dd4e521a4339352d2ee30d611c31131b
Richard Neumann <mail@richard-neumann.de>
2021-04-29 15:39:18 +0200

    Implement frontend

---
+++ frontend/cleaninglog.mjs
@@ -0,0 +1,32 @@
+/*
+    cleaninglog.mjs - Cleaning log JavaScript library.
+
+    (C) 2021 HOMEINFO - Digitale Informationssysteme GmbH
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Maintainer: Richard Neumann <r dot neumann at homeinfo period de>
+*/
+'use strict';
+
+import { request } from 'https://javascript.homeinfo.de/his/his.mjs';
+
+export const BASE_URL = 'https://backend.homeinfo.de/cleaninglog';
+
+
+/*
+    Synchronizes the respective system.
+*/
+export function init (system) {
+}
